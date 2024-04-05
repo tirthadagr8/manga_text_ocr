b63b2e3271f51b4f9941fd7a997181012d7aaaf7
Tirthankar Ghosh <55987952+tirthadagr8@users.noreply.github.com>
2024-04-05 20:58:31 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -5,17 +5,27 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
-op = webdriver.ChromeOptions()
-op.add_argument('headless')
-driver = webdriver.Chrome(options=op)
+
+brave_path = "C://Program Files//BraveSoftware//Brave-Browser//Application//brave.exe"
+# Define Chrome options
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = brave_path
+chrome_options.add_argument('headless')
+
+# Initialize the webdriver without specifying executable path
+driver = webdriver.Chrome(options=chrome_options)
 def fetch_data(s):
     link='https://www.deepl.com/en/translator#ja/en/'+s
     driver.get(link)
     sol=WebDriverWait(driver,10).until(
         EC.presence_of_element_located(
-        (By.XPATH,"//div[@title='Click a word to see alternative translations']")
+        (By.XPATH,"//span[@class='--l --r sentence_highlight']")
         )
         )
-    return sol.text
+    temp=driver.find_elements(By.XPATH,"//div[@lang='en-US']")
+    s=''
+    for t in temp:
+        s+=t.text
+    return s
 
 # print(fetch_data('体力が回復したわけではあるまい？'))
